226dbb9e0c6dfc7ef9850b8d521aee99c75ae073
Subbu-2607 <164450990+Subbu-2607@users.noreply.github.com>
2024-06-08 11:35:33 +0530

    Add files via upload

---
+++ Backend/chat.gateway.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+@WebSocketGateway({ cors: '*' })
+export class ChatGateway {
+  @WebSocketServer()
+  server;
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    console.log(message);
+    this.server.emit('message', message);
+  }
+}
